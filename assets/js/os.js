a5ff68bbff2a23fba54d6c7f66d9265cc6c17a33
finglis <42358257+finglis@users.noreply.github.com>
2023-11-10 13:25:40 +0000

    mac intel detection

--- assets/js/os.js
+++ assets/js/os.js
@@ -2,10 +2,19 @@
   var htmlElement = document.documentElement;
 
   if (navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)) {
-    htmlElement.className = "mac";
+    if (navigator.platform == "MacIntel") {
+      htmlElement.className = "macIntel";
+    } else {
+      htmlElement.className = "macAll";
+    }
+    console.log(navigator.platform);
   } else if (navigator.platform.match(/(Win16|Win32|Android)/i)) {
     htmlElement.className = "win";
-  } else if (navigator.platform.match(/(Linux i686|Linux armv7l|Linux x86_64|Linux x86_64)/i)) {
+  } else if (
+    navigator.platform.match(
+      /(Linux i686|Linux armv7l|Linux x86_64|Linux x86_64)/i
+    )
+  ) {
     htmlElement.className = "lin";
   } else {
     htmlElement.className = "default";
