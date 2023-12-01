aa0d181e451a60ab987ec877b9d03f8bead6a69c
finglis <42358257+finglis@users.noreply.github.com>
2023-12-01 17:02:53 +0000

    intel/silicon detection logic removed

--- assets/js/os.js
+++ assets/js/os.js
@@ -2,13 +2,15 @@
   var htmlElement = document.documentElement;
 
   if (navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)) {
-    if (navigator.platform == "MacIntel") {
-      htmlElement.className = "macIntel";
-    } else {
+    // console.log(navigator.platform);
+    // if (navigator.platform == "MacIntel") {
+    //   htmlElement.className = "macIntel";
+    // } else {
       htmlElement.className = "macAll";
-    }
+    // }
   } else if (navigator.platform.match(/(Win16|Win32|Android)/i)) {
     htmlElement.className = "win";
+    console.log(navigator.platform);
   } else if (
     navigator.platform.match(
       /(Linux i686|Linux armv7l|Linux x86_64|Linux x86_64)/i
